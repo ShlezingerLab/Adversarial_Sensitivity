fbdaaf8ddf18041f566f6f7bbb3211b67b8d08e2
Elad Sofer <elad.g.sofer@gmail.com>
2023-05-17 03:06:29 +0300

    main.py path creation support

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 __author__ = 'Elad Sofer <elad.g.sofer@gmail.com>'
 
 import argparse
+import os
+
 import ista, admm, beamforming_attack
+from utills import FIGURES_PATH, MATRICES_PATH
 
 if __name__ == '__main__':
 
@@ -14,6 +17,13 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--ista', action='store_true')
     parser.add_argument('-a', '--admm', action='store_true')
     args = parser.parse_args()
+
+    # Creating needed paths
+    if not os.path.exists(FIGURES_PATH):
+        os.mkdir(FIGURES_PATH)
+
+    if not os.path.exists(MATRICES_PATH):
+        os.mkdir(MATRICES_PATH)
 
     if args.beamforming:
         beamforming_attack.execute()
